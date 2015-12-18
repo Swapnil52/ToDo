d1132a57c01c2341b8c62d4a77c4932298d80726
Swapnil Beta <swapnildhanwal@hotmail.com>
2015-12-18 23:57:52 +0530

    Initial Commit

---
+++ ToDo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ToDo
+//
+//  Created by Swapnil Dhanwal on 18/12/15.
+//  Copyright © 2015 SwApp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
